e492e873470670e1e98c3c2519646db71b42c1c0
Sami-Wafula <samiwafula3257@gmail.com>
2022-11-04 11:35:41 +0300

    Question 1

---
+++ checkPalindrome.js
@@ -0,0 +1,12 @@
+function checkPalindrome(x){
+    
+    let string = String(x); 
+
+    if (string[0] === string[(string.length-1)]){
+        console.log('true')
+    } else {
+        console.log('false')
+    }
+}
+
+
